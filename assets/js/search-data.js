2cc1e0f7db6303548bffdea0314d377f1d898321
lizhongzheng <lizhong@mit.edu>
2025-01-27 15:02:47 +0000

    Deploying to gh-pages from @ lizhongzheng/lizhongzheng.github.io@bea8608c6e998dd09ed5b46b2171e1f385aecf2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,28 +58,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "dropdown-publications",
-              title: "publications",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
-            },{id: "dropdown-projects",
-              title: "projects",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-image-galleries",
+        },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
@@ -431,18 +410,20 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+    },{id: "news-i-gave-a-keynote-at-icccn-2024",
+          title: 'I gave a keynote at ICCCN 2024.',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+              window.location.href = "/news/announcement_4/";
+            },},{id: "news-i-gave-a-seminar-talk-at-the-ece-department-of-purdue-university",
+          title: 'I gave a seminar talk at the ECE department of Purdue University.',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-i-served-as-a-tpc-co-chair-for-itw-2024",
+          title: 'I served as a TPC co-chair for ITW 2024.',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_6/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
